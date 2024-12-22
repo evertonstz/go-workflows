6fb82b8f1915858c8cfaa1629b1c0a4c699dc049
evertonstz <evertonjcorreia@gmail.com>
2024-12-21 21:30:59 -0300

    implement split view

--- main.go
+++ main.go
@@ -1,45 +1,113 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"math"
 
 	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
+	"github.com/evertonstz/go-workflows/components/footer"
 	"github.com/evertonstz/go-workflows/components/form"
 	"github.com/evertonstz/go-workflows/components/list"
-	"github.com/evertonstz/go-workflows/components/footer"
 	"github.com/evertonstz/go-workflows/shared"
 )
 
+type sessionState uint
+
+const (
+	leftView sessionState = iota
+	rightView
+)
+
+var (
+	modelStyle = lipgloss.NewStyle().
+			Width(15).
+			Height(5).
+			Align(lipgloss.Center, lipgloss.Center).
+			BorderStyle(lipgloss.HiddenBorder())
+	focusedModelStyle = lipgloss.NewStyle().
+				Width(15).
+				Height(5).
+				Align(lipgloss.Center, lipgloss.Center).
+				BorderStyle(lipgloss.NormalBorder()).
+				BorderForeground(lipgloss.Color("69"))
+)
+
+type termDimensions struct {
+	width  int
+	height int
+}
+
 type model struct {
-	list   tea.Model
-	form   form.Model
-	footer footer.Model
+	state          sessionState
+	list           tea.Model
+	form           form.Model
+	footer         footer.Model
+	termDimensions termDimensions
 }
 
 func (m model) Init() tea.Cmd {
 	return nil
 }
 
+func (m model) focused() sessionState {
+	return m.state
+}
+
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-	var cmdList, cmdForm, cmdFooter tea.Cmd
+	var cmds []tea.Cmd
+	// var cmdForm tea.Cmd
 
 	switch msg := msg.(type) {
-    case tea.KeyMsg:
+	case tea.WindowSizeMsg:
+		m.termDimensions.width = msg.Width
+		m.termDimensions.height = msg.Height
+	case tea.KeyMsg:
 		switch msg.String() {
 		case "ctrl+c", "q":
-			return m, tea.Quit}
+			return m, tea.Quit
+		case "tab":
+			if m.state == leftView {
+				m.state = rightView
+			} else {
+				m.state = leftView
+			}
+		}
 	case shared.SelectedItemMsg:
-		m.form, cmdForm = m.form.Update(msg)
+		m.form, _ = m.form.Update(msg)
 	}
 
-	m.list, cmdList = m.list.Update(msg)
-	m.footer, cmdFooter = m.footer.Update(msg)
+	switch m.focused() {
+	case leftView:
+		var c tea.Cmd
+		m.list, c = m.list.Update(msg)
+		cmds = append(cmds, c)
+	case rightView:
+		var c tea.Cmd
+		m.form, c = m.form.Update(msg)
+		cmds = append(cmds, c)
+	}
 
-	return m, tea.Batch(cmdList, cmdForm, cmdFooter)
+	// m.list, cmdList = m.list.Update(msg)
+	// m.footer, cmdFooter = m.footer.Update(msg)
+
+	return m, tea.Batch(cmds...)
 }
 
 func (m model) View() string {
-	return m.list.View() + "\n" + m.form.View() + "\n" + m.footer.View()
+	fistPanelWidth := int(math.Floor(float64(m.termDimensions.width) * 0.5))
+	secondPanelWidth := m.termDimensions.width - fistPanelWidth - 4
+	panelHeight := m.termDimensions.height - 5
+	if m.focused() == leftView {
+		return lipgloss.JoinHorizontal(lipgloss.Left, 
+			focusedModelStyle.Width(fistPanelWidth).Height(panelHeight).Render(fmt.Sprintf("%4s", m.list.View())), 
+			modelStyle.Width(secondPanelWidth).Height(panelHeight).Render(m.form.View()))
+	} else {
+		return lipgloss.JoinHorizontal(lipgloss.Left, 
+			modelStyle.Width(fistPanelWidth).Height(panelHeight).Render(fmt.Sprintf("%4s", m.list.View())), 
+			focusedModelStyle.Width(secondPanelWidth).Height(panelHeight).Render(m.form.View()))
+	}
 }
 
 func main() {
@@ -49,7 +117,7 @@ func main() {
 		footer: footer.New(),
 	}
 
-	p := tea.NewProgram(m)
+	p := tea.NewProgram(m, tea.WithAltScreen())
 
 	if err := p.Start(); err != nil {
 		log.Fatalf("Error starting app: %v", err)
