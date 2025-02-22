2a875492067f6d1e5fb4b44f63ce7ba3bb4fbce5
evertonstz <evertonjcorreia@gmail.com>
2025-02-22 17:21:53 -0300

    refactor: simplify Model state management and remove unused code

--- components/list/list.go
+++ components/list/list.go
@@ -1,7 +1,6 @@
 package list
 
 import (
-	"strings"
 	"time"
 
 	"github.com/charmbracelet/bubbles/key"
@@ -19,11 +18,6 @@ type myItem struct {
 	dateUpdated          time.Time
 }
 
-const (
-	addNewOff inputs = iota
-	addNewOn
-)
-
 var docStyle = lipgloss.NewStyle()
 
 func (i myItem) Title() string          { return i.title }
@@ -34,14 +28,9 @@ func (i myItem) DateUpdated() time.Time { return i.dateUpdated }
 func (i myItem) FilterValue() string    { return i.title }
 
 type Model struct {
-	state           inputs
 	inputs          inputsModel
 	list            list.Model
 	lastSelectedIdx int
-}
-
-func (m Model) InputOn() bool {
-	return m.state == addNewOn
 }
 
 func (m Model) CurentItem() myItem {
@@ -64,14 +53,6 @@ func (m Model) Init() tea.Cmd {
 	return nil
 }
 
-func (m *Model) showAddNew() {
-	m.state = addNewOn
-}
-
-func (m *Model) hideAddNew() {
-	m.state = addNewOff
-}
-
 func (m *Model) SetSize(width, height int) {
 	h, v := docStyle.GetFrameSize()
 	m.list.SetSize(width-h, height-v)
@@ -92,9 +73,6 @@ func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	var cmds []tea.Cmd
 	switch msg := msg.(type) {
 	case shared.DidAddNewItemMsg:
-		// if m.inputs.Title.Value() == "" || m.inputs.Description.Value() == "" {
-		// 	return m, nil
-		// }
 		newItem := myItem{
 			title:       msg.Title,
 			desc:        msg.Description,
@@ -103,7 +81,6 @@ func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 			dateUpdated: time.Now(),
 		}
 		m.list.InsertItem(len(m.list.Items()), newItem)
-		// m.hideAddNew()
 		return m, nil
 	case shared.DidDeleteItemMsg:
 		m.list.RemoveItem(msg.Index) 
@@ -143,16 +120,6 @@ func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		cmds = m.setCurrentItemCmd(cmds)
 	case tea.KeyMsg:
 		switch {
-		case key.Matches(msg, Keys.Esc):
-			if m.state == addNewOn {
-				m.hideAddNew()
-				return m, nil
-			}
-		// case key.Matches(msg, Keys.AddNewWorkflow):
-		// 	if m.state == addNewOff {
-		// 		m.showAddNew()
-		// 		return m, nil
-		// 	}
 		case key.Matches(msg, Keys.CopyWorkflow):
 			selectedItem := m.list.SelectedItem()
 			if selectedItem != nil {
@@ -160,62 +127,26 @@ func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 					return m, shared.CopyToClipboardCmd(selected.command)
 				}
 			}
-		case key.Matches(msg, Keys.Enter):
-			if m.state == addNewOff {
-				return m, shared.SetCurrentItemCmd(models.Item{
-					Title:       m.CurentItem().Title(),
-					Desc:        m.CurentItem().Description(),
-					Command:     m.CurentItem().Command(),
-					DateAdded:   m.CurentItem().DateAdded(),
-					DateUpdated: m.CurentItem().DateUpdated()},
-				)
-			}
-			var c tea.Cmd
-			m.inputs, c = m.inputs.Update(msg)
-			return m, c
 		}
 	}
 
-	switch m.state {
-	case addNewOff:
-		var c tea.Cmd
-		m.list, c = m.list.Update(msg)
-		cmds = append(cmds, c)
-		if m.list.Index() != m.lastSelectedIdx {
-			cmds = m.setCurrentItemCmd(cmds)
-			m.lastSelectedIdx = m.list.Index()
-		}
-	case addNewOn:
-		var c tea.Cmd
-		m.inputs, c = m.inputs.Update(msg)
-		cmds = append(cmds, c)
+	var c tea.Cmd
+	m.list, c = m.list.Update(msg)
+	cmds = append(cmds, c)
+	if m.list.Index() != m.lastSelectedIdx {
+		cmds = m.setCurrentItemCmd(cmds)
+		m.lastSelectedIdx = m.list.Index()
 	}
 
 	return m, tea.Batch(cmds...)
 }
 
 func (m Model) View() string {
-	var v string
-	// listView := docStyle.Render(m.list.View())
-	inputView := m.inputs.View()
-	inputViewHeight := strings.Count(inputView, "\n")
-	switch m.state {
-	case addNewOff:
-		v = docStyle.Render(m.list.View())
-		// return  m.list.View()
-	case addNewOn:
-		v = lipgloss.JoinVertical(lipgloss.Top,
-			docStyle.
-				Height(m.list.Height()-inputViewHeight).
-				Render(m.list.View()),
-			inputView)
-	}
-
-	return v
+	return docStyle.Render(m.list.View())
 }
 
 func New() Model {
-	m := Model{list: list.New([]list.Item{}, list.NewDefaultDelegate(), 0, 0), inputs: newInputsModel(), state: addNewOff}
+	m := Model{list: list.New([]list.Item{}, list.NewDefaultDelegate(), 0, 0), inputs: newInputsModel()}
 	m.list.SetShowTitle(false)
 	m.list.SetShowHelp(false)
 	m.Init()
